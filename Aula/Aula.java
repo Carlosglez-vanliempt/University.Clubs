d8de658eefb163cf6da57a5e5f81b33f0bd41d9c
Alvaro Farreny <farrenyalvaro@gmail.com>
2021-05-29 17:29:34 +0200

    Clase Aula
    Clase aula creada y subida

---
+++ Aula/Aula.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Aula {
+
+    private int id;
+    private String nombre;
+    private int capacidad;
+    private double localizacion;
+
+    public Aula(int id, String nombre, int capacidad, double localizacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.localizacion = localizacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" +
+                "id= " + id +
+                ", nombre= '" + nombre + '\'' +
+                ", capacidad= " + capacidad +
+                ", localizacion= " + localizacion +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(double localizacion) {
+        this.localizacion = localizacion;
+    }
+}
